2e328961fe8c90b8dc4d5694614201c9cd3464f7
nshib00 <nshib.54321@gmail.com>
2024-10-27 18:30:06 +0300

    Setup docker-compose, add bash scripts

--- hospital/docker/app.sh
+++ hospital/docker/app.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 alembic upgrade head
-gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8082
+gunicorn app.main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8082
